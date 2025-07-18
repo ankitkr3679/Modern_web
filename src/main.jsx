274a0f2bd527638aeb5d7c2a42fbce20412a5046
ankitkr3679 <166656461+ankitkr3679@users.noreply.github.com>
2025-07-19 00:09:31 +0530

    fourth

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/Modern_web">
       <App />
     </BrowserRouter>
   </React.StrictMode>
